39c39442d2418f74ba53151ae9d9704f8f303791
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2020-05-13 17:13:15 +0530

    Add root route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.listen(port, () => {
     console.log('Listening!', port);
 });
 
+app.get('/', async (req, res) => {
+    res.send('Rotify!');
+});
+
 app.get('/hello', async (req, res) => {
     const name = req.query.name;
     const result = `Hello ${name}\n\n`;
