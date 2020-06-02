a46623c037272af7b415e7f914bbe338182b3ce9
Jagdeep Singh <jdecode@gmail.com>
2020-06-02 19:54:57 +0530

    Testing changes + moving API to GCP : probability of not working is high

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/hello', async (req, res) => {
 */
 
 app.get('/events', async (req, res) => {
-    const events = await getRow();
+    const events = await getApi('events');
     let retval;
     if (events) {
         retval = {
@@ -33,50 +33,56 @@ app.get('/events', async (req, res) => {
             data: { events: 'nothing' }
         }
     }
+    getRes(retval);
+});
+
+function getRes(retval) {
     res.setHeader('content-type', 'application/json');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,GET');
     res.send(JSON.stringify(retval));
-});
+}
 
-async function getRow() {
+async function getApi(type = 'events') {
     const auth = await google.auth.getClient({
         scopes: ['https://www.googleapis.com/auth/spreadsheets']
     });
 
     const api = google.sheets({ version: 'v4', auth });
-    const response = await api.spreadsheets.values.get({
-        spreadsheetId: '1e2GXQAvCEeJ-iUtQzTCSI_US-6Hh1K_22rYbbokyzj0',
-        range: 'Events!A:F'
-    });
+    if(type == 'events') {
+        const response = await api.spreadsheets.values.get({
+            spreadsheetId: '1e2GXQAvCEeJ-iUtQzTCSI_US-6Hh1K_22rYbbokyzj0',
+            range: 'Events!A:F'
+        });
 
-    let frow = true;
-    let past = [];
-    let upcoming = [];
-    for (let row of response.data.values) {
-	if(frow) {
-	    frow = false;
-	    continue;
-	}
-	if(row[5] == 'Disabled') {
-	    continue;
-	}
-	if(row[3] == 'Past') {
-            past.push({
+        let frow = true;
+        let past = [];
+        let upcoming = [];
+        for (let row of response.data.values) {
+            if(frow) {
+                frow = false;
+                continue;
+            }
+            if(row[5] == 'Disabled') {
+                continue;
+            }
+            if(row[3] == 'Past') {
+                    past.push({
+                        title: row[0],
+                        date: row[1],
+                        speaker: row[2],
+                        link: row[4]
+                    });
+            } else {
+                upcoming.push({
                 title: row[0],
                 date: row[1],
                 speaker: row[2],
                 link: row[4]
-            });
-	} else {
-	    upcoming.push({
-		title: row[0],
-		date: row[1],
-		speaker: row[2],
-		link: row[4]
-	    });
-	}
+                });
+            }
+        }
+        return {past: past, upcoming: upcoming};
     }
-    return {past: past, upcoming: upcoming};
 }
